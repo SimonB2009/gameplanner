de2a0a1859dd350bf4427b33ae0b028abeda12ee
SimonB2009 <brandstetter.simon@web.de>
2023-01-26 09:43:46 +0100

    feat: added first saving function

--- main.cs
+++ main.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Net;
 using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Softplanner
 {   
@@ -11,6 +14,7 @@ namespace Softplanner
          static string action;
          static bool restart = false;
          static void Main(string[] args) {
+            load();
             start();
          }
 
@@ -127,32 +131,32 @@ namespace Softplanner
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("      Name :  ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(name);
+            Console.WriteLine(test[1]);
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("      Typ :  ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            //Console.WriteLine(typ);;
+            Console.WriteLine(test[0]);
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("      Discription :  ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(discription);
+            Console.WriteLine(test[4]);
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("      Programmer :  ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(programmer);
+            Console.WriteLine(test[3]);
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("      from - to :  ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(fromto);
+            Console.WriteLine(test[2]);
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("      on GitHub (yes / no) :  ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(github);
+            Console.WriteLine(test[5]);
 
             Console.WriteLine ("");
             Console.WriteLine ("");
@@ -161,7 +165,11 @@ namespace Softplanner
             Console.ForegroundColor = ConsoleColor.Blue;
             
             if (Console.ReadLine() == "yes") {
-               save();
+               
+               if (save() == false) {
+                  restart = true;                                             //TODO
+                  start();
+               }
                return;
             } else {
                Console.WriteLine("");
@@ -179,15 +187,39 @@ namespace Softplanner
          static bool save() { // save and return "true" if it worked else: "false"
             int test1 = 1;
             int test2 = 2;
-            if (test1 == test2) { //just for testing.
+            try (test1 == test2) { //just for testing. //*try: saving    Console.WriteLine("speichern");
+                                                                        //stream = new FileStream("MyFile.bin", FileMode.Open, FileAccess.Write, FileShare.None);  
+                                                                        //formatter.Serialize(stream, scores);  
+                                                                        //stream.Close(); 
                return true;
-            } else {
+            } 
+            
+            catch () {                                                                       //false
                Console.WriteLine("");
                Console.WriteLine("");
                Console.ForegroundColor = ConsoleColor.Red;
                Console.Write("      This didn t worked! Please try again.");
                return false;
             }
+         }
+
+         static void load() {
+           /* Stream stream;
+
+            IFormatter formatter = new BinaryFormatter();  
+            try {
+                stream = new FileStream(@"MyFile.bin",FileMode.Open,FileAccess.Read);
+                
+                test = (test[])formatter.Deserialize(stream);
+                stream.Close();
+            } catch(FileNotFoundException exception)
+            {
+                for (int i=0; i<10; i++) {
+                    scores[i] = new Score();
+                }
+                System.IO.FileStream fs = System.IO.File.Create("MyFile.bin");
+                fs.Close();
+            }*/
          }
      }
 }
